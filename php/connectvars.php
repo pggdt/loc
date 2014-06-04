5b2405e52f98c2b616bca3626255558fb67bc642
pggdt <inner42@gmail.com>
2014-06-04 16:07:31 +0800

    web

---
+++ php/connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'your dbhost');
+  define('DB_USER', 'your username');
+  define('DB_PASSWORD', 'your password');
+  define('DB_NAME', 'dbname');
+?>
