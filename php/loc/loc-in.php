a89ca0d0c793e95ede0e45041dc76b0fac3058d5
pggdt <inner42@gmail.com>
2014-10-26 22:23:15 +0800

    Update loc-in.php
    add mysqli_real_escape_string

--- php/loc/loc-in.php
+++ php/loc/loc-in.php
@@ -22,6 +22,17 @@ if (mysqli_connect_errno($dbc))
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 mysqli_set_charset($dbc, "utf8") or die('Could not connect: ' . mysqli_error($dbc));
+$latitude=mysqli_real_escape_string($dbc,$latitude);
+$longitude=mysqli_real_escape_string($dbc,$longitude);
+$accuracy=mysqli_real_escape_string($dbc,$accuracy);
+$altitude=mysqli_real_escape_string($dbc,$altitude);
+$altitudeAccuracy=mysqli_real_escape_string($dbc,$altitudeAccuracy);
+$heading=mysqli_real_escape_string($dbc,$heading);
+$speed=mysqli_real_escape_string($dbc,$speed);
+$timestamp=mysqli_real_escape_string($dbc,$timestamp);
+$time=mysqli_real_escape_string($dbc,$time);
+$text=mysqli_real_escape_string($dbc,$text);
+$geoCode=mysqli_real_escape_string($dbc,$geoCode);
 $query="insert into locTest (ID,user_id,latitude,longitude,accuracy,altitude,altitudeAccuracy,heading,speed,locTimestamp,locTime,text,geoCode,insertTime) values (NULL,'$user_id','$latitude','$longitude','$accuracy','$altitude','$altitudeAccuracy','$heading','$speed','$timestamp','$time','$text','$geoCode',UTC_TIMESTAMP())";
 $result = mysqli_query($dbc,$query) or die(mysqli_error($dbc));
 mysqli_close($dbc);
