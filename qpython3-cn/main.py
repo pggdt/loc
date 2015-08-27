b6cff07dabe41d74ec2fb149b10ce7d2e86e93bb
pggdt <inner42@gmail.com>
2015-08-27 09:54:12 +0800

    add audio record

--- qpython3-cn/main.py
+++ qpython3-cn/main.py
@@ -1,80 +1,92 @@
-#qpy:gui
-from android import Android
-import sys,time,socket,urllib,http.cookiejar,sqlite3,json,hashlib
-droid = Android()
-geoData={ 'latitude':'0','longitude':'0','accuracy':'0','altitude':'0','heading':'0','speed':'0','timestamp':'0','readTime':'0','text':'','geoCode':''}
-r1={}
-LANG='zh-TW'
-#zh-TW en
-ac=''
-ps=''
-SALT='yourSalt'
-locBaseDir="/storage/sdcard0/com.hipipal.qpyplus/projects3/qpython3-cn/"
-locBaseUrl='http://test.test.info/'
+# qpy:gui
+# from android import Android
+
+import sl4a, sys, time, socket, urllib, http.cookiejar, sqlite3, json, hashlib
+droid = sl4a.Android()
+geoData = { 'latitude':'0', 'longitude':'0', 'accuracy':'0', 'altitude':'0', 'heading':'0', 'speed':'0', 'timestamp':'0', 'readTime':'0', 'text':'', 'geoCode':''}
+r1 = {}
+LANG = 'zh-TW'
+# zh-TW en
+ac = ''
+ps = ''
+SALT = 'yourSalt'
+locBaseDir = "/storage/sdcard0/com.hipipal.qpyplus/projects3/qpython3-cn/"
+locBaseUrl = 'https://test.test.info/'
 
 def upload():
-  del_ID=[]
-  conn = sqlite3.connect(locBaseDir+'/data/loc.db')
+  del_ID = []
+  conn = sqlite3.connect(locBaseDir + '/data/loc.db')
   c = conn.cursor()
   c.execute ("""select * from LocalLoc""")
   while 1:
-    r=c.fetchone()
-    droid.eventPost('insert',str(len(del_ID))+'fetched')
-    if r!=None:
-      rs=doPost("""$_SESSION["user_id"]""",r[1],r[2],r[3],r[4],'0',r[5],r[6],r[7],r[8],r[9],r[10])
-      rs1='Record at '+r[8]+' added.'
-      if len(rs)==len(rs1)+1:
+    r = c.fetchone()
+    droid.eventPost('insert', str(len(del_ID)) + 'fetched')
+    if r != None:
+      rs = doPost("""$_SESSION["user_id"]""", r[1], r[2], r[3], r[4], '0', r[5], r[6], r[7], r[8], r[9], r[10])
+      rs1 = 'Record at ' + r[8] + ' added.'
+      if len(rs) == len(rs1) + 1:
         del_ID.append(r[0])
       else:
-      	print('delete id='+str(r[0])+' failed')
+          print('delete id=' + str(r[0]) + ' failed')
     else:
       break
   for i in del_ID:
-    c.execute ("""delete from LocalLoc where ID="""+str(i))
+    c.execute ("""delete from LocalLoc where ID=""" + str(i))
   conn.commit()
-  droid.eventPost('insert',str(len(del_ID))+'items uploaded')
+  droid.eventPost('insert', str(len(del_ID)) + 'items uploaded')
   c.close()
 
-def setGeoData(location,code):
-  global geoData
-  timeS= time.localtime(location['time']/1000)
-  myreadTime = time.strftime("%Y-%m-%d %H:%M:%S",timeS)
-  geoData['readTime']=myreadTime
-  geoData['provider']=str(location['provider'])
-  geoData['latitude']='{0:.8f}'.format(location['latitude'])
-  geoData['longitude']='{0:.10f}'.format(location['longitude'])
-  geoData['accuracy']=str(int(location['accuracy']))
-  if location['altitude']!=0:
-    geoData['altitude']='{0:.4f}'.format(location['altitude'])
-  else:
-    geoData['altitude']='0'
-  if location['speed']!=0:
-    geoData['speed']='{0:.4f}'.format(location['speed'])
-  else:
-    geoData['speed']='0'
-  if location['bearing']!=0:
-    geoData['heading']='{0:.2f}'.format(location['bearing'])
-  else:
-    geoData['heading']='0'
-  geoData['timestamp']=str(location['time'])
-  if code=="noCode":
-    geoData['geoCode']='-'+geoData['provider']
-  else:
-    geoData['geoCode']=getgeocode(code)+'-'+geoData['provider']
+      
+def startRecordAudio():
+  des = locBaseDir + "data/audio/" + geoData['timestamp'] + ".wav"
+  print(des)
+  droid.recorderStartMicrophone(des)
+  # time.sleep(10)
 
-def doPost(userId,latitude,longitude,accuracy,altitude,altitudeAccuracy,heading,speed,locTimestamp,locTime,textNote='',geoCode=""):  
-  data = urllib.parse.urlencode({'user_id':userId,'latitude': latitude ,'longitude':longitude,'accuracy':accuracy,'altitude':altitude,'altitudeAccuracy':altitudeAccuracy,'heading':heading,'speed':speed,'timestamp':locTimestamp,'time':locTime,'text':textNote,'geoCode':geoCode})
-  data=data.encode('utf-8')
-  request = urllib.request.Request(locBaseUrl+'loc/loc-in.php',data)
+def stopRecord():
+  droid.recorderStop()
+  print("record ok")
+
+def setGeoData(location, code):
+  global geoData
+  timeS = time.localtime(location['time'] / 1000)
+  myreadTime = time.strftime("%Y-%m-%d %H:%M:%S", timeS)
+  geoData['readTime'] = myreadTime
+  geoData['provider'] = str(location['provider'])
+  geoData['latitude'] = '{0:.8f}'.format(location['latitude'])
+  geoData['longitude'] = '{0:.10f}'.format(location['longitude'])
+  geoData['accuracy'] = str(int(location['accuracy']))
+  if location['altitude'] != 0:
+    geoData['altitude'] = '{0:.4f}'.format(location['altitude'])
+  else:
+    geoData['altitude'] = '0'
+  if location['speed'] != 0:
+    geoData['speed'] = '{0:.4f}'.format(location['speed'])
+  else:
+    geoData['speed'] = '0'
+  if location['bearing'] != 0:
+    geoData['heading'] = '{0:.2f}'.format(location['bearing'])
+  else:
+    geoData['heading'] = '0'
+  geoData['timestamp'] = str(location['time'])
+  if code == "noCode":
+    geoData['geoCode'] = '-' + geoData['provider']
+  else:
+    geoData['geoCode'] = getgeocode(code) + '-' + geoData['provider']
+
+def doPost(userId, latitude, longitude, accuracy, altitude, altitudeAccuracy, heading, speed, locTimestamp, locTime, textNote='', geoCode=""):  
+  data = urllib.parse.urlencode({'user_id':userId, 'latitude': latitude , 'longitude':longitude, 'accuracy':accuracy, 'altitude':altitude, 'altitudeAccuracy':altitudeAccuracy, 'heading':heading, 'speed':speed, 'timestamp':locTimestamp, 'time':locTime, 'text':textNote, 'geoCode':geoCode})
+  data = data.encode('utf-8')
+  request = urllib.request.Request(locBaseUrl + 'loc/loc-in.php', data)
   try:
     response = urllib.request.urlopen(request)
-  except (urllib.error.HTTPError, socket.error,urllib.error.URLError) as e:
+  except (urllib.error.HTTPError, socket.error, urllib.error.URLError) as e:
     print('Connection error occurred when inserting data.')
     return saveToLocal()
   else:
     r = response.read().decode('utf-8')
     if response.code != 200:
-      print("Error code:"+response.code)  
+      print("Error code:" + response.code)  
     else:
       print(r)
       return r
@@ -82,169 +94,170 @@ def doPost(userId,latitude,longitude,accuracy,altitude,altitudeAccuracy,heading,
 def checkAc(c=0):
   global ac
   global ps
-  if c==0:
-    a= droid.prefGetAll("z").result
+  if c == 0:
+    a = droid.prefGetAll("z").result
     if ('ac' in a):
-      ac=a['ac']
-      ps=a['ps']
+      ac = a['ac']
+      ps = a['ps']
     else:
-      line = droid.dialogGetInput('Account Info','Please input your account:')
-      ac=str(line.result)
-      droid.prefPutValue("ac",ac,"z")
-      line1 = droid.dialogGetPassword('Account Info','Please input your password:')
-      ps=str(line1.result)+SALT
-      ps=ps.encode('utf-8')
-      ps=hashlib.sha256(ps).hexdigest()
-      droid.prefPutValue("ps",ps,"z")
-      droid.makeToast('Account '+ac+' saved')
-  elif c==1:
-      line = droid.dialogGetInput('Account Info','Please input your account:')
-      ac=str(line.result)
-      droid.prefPutValue("ac",ac,"z")
-      line1 = droid.dialogGetPassword('Account Info','Please input your password:')
-      ps=str(line1.result)
-      droid.prefPutValue("ps",ps,"z")
-      droid.makeToast('Account '+ac+' saved')
+      line = droid.dialogGetInput('Account Info', 'Please input your account:')
+      ac = str(line.result)
+      droid.prefPutValue("ac", ac, "z")
+      line1 = droid.dialogGetPassword('Account Info', 'Please input your password:')
+      ps = str(line1.result) + SALT
+      ps = ps.encode('utf-8')
+      ps = hashlib.sha256(ps).hexdigest()
+      droid.prefPutValue("ps", ps, "z")
+      droid.makeToast('Account ' + ac + ' saved')
+  elif c == 1:
+      line = droid.dialogGetInput('Account Info', 'Please input your account:')
+      ac = str(line.result)
+      droid.prefPutValue("ac", ac, "z")
+      line1 = droid.dialogGetPassword('Account Info', 'Please input your password:')
+      ps = str(line1.result)
+      droid.prefPutValue("ps", ps, "z")
+      droid.makeToast('Account ' + ac + ' saved')
 
 def doLogin():
   checkAc(0)
-  url=locBaseUrl+"login/login_go.php"
-  data=urllib.parse.urlencode({'UserName':ac,'Password':ps,'KeepInfo':'unchecked'})
-  data=data.encode('utf-8')
-  cj=http.cookiejar.CookieJar()
+  url = locBaseUrl + "login/login_go.php"
+  data = urllib.parse.urlencode({'UserName':ac, 'Password':ps, 'KeepInfo':'unchecked'})
+  data = data.encode('utf-8')
+  cj = http.cookiejar.CookieJar()
   cookie_support = urllib.request.HTTPCookieProcessor(cj)
   opener = urllib.request.build_opener(cookie_support)
   urllib.request.install_opener(opener)
-  req=urllib.request.Request(url = url,data = data)
-  rt=''
+  req = urllib.request.Request(url=url, data=data)
+  rt = ''
   try:
-    response=opener.open(req)
-  except (urllib.error.HTTPError, socket.error, socket.gaierror,urllib.error.URLError) as e:
+    response = opener.open(req)
+  except (urllib.error.HTTPError, socket.error, socket.gaierror, urllib.error.URLError) as e:
     print('Connection error, couldn\'t login in.')
     return rt
   else:
-    rt= response.read().decode('utf-8')
+    rt = response.read().decode('utf-8')
     droid.makeToast(rt)
     return rt
 
 def writeLocation(geoData):
   if (doLogin().startswith('Welcome')):
-    return doPost("""$_SESSION["user_id"]""",geoData['latitude'],geoData['longitude'],geoData['accuracy'],geoData['altitude'],'0',geoData['heading'],geoData['speed'],geoData['timestamp'],geoData['readTime'],geoData['text'],geoData['geoCode'])
+    return doPost("""$_SESSION["user_id"]""", geoData['latitude'], geoData['longitude'], geoData['accuracy'], geoData['altitude'], '0', geoData['heading'], geoData['speed'], geoData['timestamp'], geoData['readTime'], geoData['text'], geoData['geoCode'])
   else:
     return saveToLocal()
 
 def saveToLocal():
   global geoData
   droid.makeToast("Try to save data to local")
-  conn = sqlite3.connect(locBaseDir+'data/loc.db')
+  conn = sqlite3.connect(locBaseDir + 'data/loc.db')
   c = conn.cursor()
   c.execute("""create table if not exists LocalLoc (ID integer primary key not NULL,latitude double,longitude double,accuracy integer,altitude double,heading double,speed double,locTimestamp integer(13),locTime CHAR(20),textMsg TEXT,geoCode TEXT)""")
-  s="""insert into LocalLoc (ID,latitude,longitude,accuracy,altitude,heading,speed,locTimestamp,locTime,textMsg,geoCode) VALUES (NULL,"""+geoData['latitude']+','+geoData['longitude']+','+geoData['accuracy']+','+geoData['altitude']+','+geoData['heading']+','+geoData['speed']+',\''+geoData['timestamp']+'\',\''+geoData['readTime']+'\',\''+geoData['text']+'\',\''+geoData['geoCode']+'\')'
+  s = """insert into LocalLoc (ID,latitude,longitude,accuracy,altitude,heading,speed,locTimestamp,locTime,textMsg,geoCode) VALUES (NULL,""" + geoData['latitude'] + ',' + geoData['longitude'] + ',' + geoData['accuracy'] + ',' + geoData['altitude'] + ',' + geoData['heading'] + ',' + geoData['speed'] + ',\'' + geoData['timestamp'] + '\',\'' + geoData['readTime'] + '\',\'' + geoData['text'] + '\',\'' + geoData['geoCode'] + '\')'
   c.execute(s)
   conn.commit()
   c.close
-  return "Record at "+geoData['readTime']+" saved to local."
+  return "Record at " + geoData['readTime'] + " saved to local."
 
 def getgeocode(code):
   global geoData
   global r1
   droid.makeToast('Getting geocode...…')
-  s=''
+  s = ''
   socket.setdefaulttimeout(20)
-  if code=="ggCodeCN":
-    data=urllib.parse.urlencode({'lat':geoData['latitude'],'lng':geoData['longitude'],'lang':LANG,'cvt':'1' })
-    data=data.encode('utf-8')
-    req=urllib.request.Request(locBaseUrl+"loc/geoi.php",data)
+  if code == "ggCodeCN":
+    data = urllib.parse.urlencode({'lat':geoData['latitude'], 'lng':geoData['longitude'], 'lang':LANG, 'cvt':'1' })
+    data = data.encode('utf-8')
+    req = urllib.request.Request(locBaseUrl + "loc/geoi.php", data)
     try:
-      response=urllib.request.urlopen(req)
-    except (urllib.error.HTTPError, socket.error,urllib.error.URLError) as e:
-      print('geo code network failed：to'+locBaseUrl+' ggCodeCN'+e.read())
+      response = urllib.request.urlopen(req)
+    except (urllib.error.HTTPError, socket.error, urllib.error.URLError) as e:
+      print('geo code network failed：to' + locBaseUrl + ' ggCodeCN' + e.read())
     else:
-      result=response.read().decode('utf-8')
-      r=json.loads(result)
-      re=r['results'][0]['address_components']      
+      result = response.read().decode('utf-8')
+      r = json.loads(result)
+      # print(r)
+      re = r['results'][0]['address_components']      
       for i in re:
-       s=s+i["long_name"]+","
-      s=s[0:-1]
+       s = s + i["long_name"] + ","
+      s = s[0:-1]
     return s
-  elif code=="ggCode":
-    data=urllib.parse.urlencode({'lat':geoData['latitude'],'lng':geoData['longitude'] ,'lang':LANG,'cvt':'0'})
-    data=data.encode('utf-8')
-    req=urllib.request.Request(locBaseUrl+"loc/geoi.php",data)
+  elif code == "ggCode":
+    data = urllib.parse.urlencode({'lat':geoData['latitude'], 'lng':geoData['longitude'] , 'lang':LANG, 'cvt':'0'})
+    data = data.encode('utf-8')
+    req = urllib.request.Request(locBaseUrl + "loc/geoi.php", data)
     try:
-      response=urllib.request.urlopen(req)
-    except (urllib.error.HTTPError, socket.error,urllib.error.URLError):
-      print('geo code network failed：to'+locBaseUrl)
+      response = urllib.request.urlopen(req)
+    except (urllib.error.HTTPError, socket.error, urllib.error.URLError):
+      print('geo code network failed：to' + locBaseUrl)
     else:
-      result=response.read().decode('utf-8')
-      r=json.loads(result)
-      re=r['results'][0]['address_components']
+      result = response.read().decode('utf-8')
+      r = json.loads(result)
+      re = r['results'][0]['address_components']
       for i in re:
-       s=s+i["long_name"]+","
-      s=s[0:-1]
+       s = s + i["long_name"] + ","
+      s = s[0:-1]
     return s
-  elif code=="zdCode":
-    data3=urllib.parse.urlencode({'lat':geoData['latitude'],'lng':geoData['longitude']})
+  elif code == "zdCode":
+    data3 = urllib.parse.urlencode({'lat':geoData['latitude'], 'lng':geoData['longitude']})
     try:
-      response1 = urllib.request.urlopen('http://api.zdoz.net/geo2loc.aspx?'+data3)
+      response1 = urllib.request.urlopen('http://api.zdoz.net/geo2loc.aspx?' + data3)
     except (urllib.error.HTTPError, socket.error) as e:
-      print('Connection error occurred when translating data from zdoz.net.' +e.read() )
+      print('Connection error occurred when translating data from zdoz.net.' + e.read())
     else:
       results_zd = response1.read().decode('utf-8')
       return results_zd
-  elif code=="ggCodeOrg":
+  elif code == "ggCodeOrg":
     try:
-      result=droid.geocode( geoData['latitude'], geoData['longitude'], 1).result
+      result = droid.geocode(geoData['latitude'], geoData['longitude'], 1).result
     except java.io.IOException:
       print('geo code network failed：java.io.IOException')
-    if result==None:
-      s=""
+    if result == None:
+      s = ""
     else:
-      result=result[0]
-      if ('thoroughfare' in result.keys()) and ('feature_name' in result.keys()) and (result['thoroughfare']==result['feature_name']):
+      result = result[0]
+      if ('thoroughfare' in result.keys()) and ('feature_name' in result.keys()) and (result['thoroughfare'] == result['feature_name']):
         del result['feature_name']
       for k in result:
-        s=s+","+str(result[k])
-      s=s[1:]
+        s = s + "," + str(result[k])
+      s = s[1:]
     return s
 
 def getGeoInfo(code):
   while True:
     try:
       """droid.readLocation().result"""
-      event=droid.eventWaitFor('location',15000).result
+      event = droid.eventWaitFor('location', 15000).result
       if ('gps' in event['data']):
-        location=event['data']['gps']
-        setGeoData(location,code)
+        location = event['data']['gps']
+        setGeoData(location, code)
         break
       elif ('fused' in event['data']):
-        location=event['data']['fused']
-        setGeoData(location,code)
+        location = event['data']['fused']
+        setGeoData(location, code)
         break
       elif ('network' in event['data']):
-        location=event['data']['network']
-        setGeoData(location,code)
+        location = event['data']['network']
+        setGeoData(location, code)
         break
       else:
         print ("No location data type")
     except :
       print ("No location data!")
       break
-  droid.eventPost('stdout',str(geoData))
+  droid.eventPost('stdout', str(geoData))
 #  print(str(geoData))
 
 def saveGeoInfo(massage):
   global geoData
-  geoData['text']=massage
-  if(geoData['latitude']=='0'):
-    geoData['time']=int(time.time()*1000)
-    timeS= time.localtime(geoData['time']/1000)
-    myreadTime = time.strftime("%Y-%m-%d %H:%M:%S",timeS)
-    geoData['readTime']=myreadTime
-    geoData['timestamp']=str(geoData['time'])
+  geoData['text'] = massage
+  if(geoData['latitude'] == '0'):
+    geoData['time'] = int(time.time() * 1000)
+    timeS = time.localtime(geoData['time'] / 1000)
+    myreadTime = time.strftime("%Y-%m-%d %H:%M:%S", timeS)
+    geoData['readTime'] = myreadTime
+    geoData['timestamp'] = str(geoData['time'])
   return writeLocation(geoData)  
 
-droid.webViewShow ('file://'+locBaseDir+'index.html')
+droid.webViewShow ('file://' + locBaseDir + 'index.html')
 while True:
   event = droid.eventWait().result
   if event ['name' ] == 'kill' :
@@ -256,9 +269,13 @@ while True:
     getGeoInfo(event [ 'data' ])
   elif event ['name' ] == 'line' :
     droid.eventPost('insert', saveGeoInfo(event [ 'data' ]))
+  elif event ['name' ] == 'startRecordAudio' :
+    startRecordAudio()
+  elif event ['name' ] == 'stopRecord' :
+    stopRecord()
   elif event ['name' ] == 'upload' :
-    lo=doLogin()
+    lo = doLogin()
     if (lo.startswith('Welcome')):
       upload()
     else:
-      droid.eventPost('insert',"Login in failed.")
+      droid.eventPost('insert', "Login in failed.")
